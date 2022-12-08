eac32ba956df5769b48d607b608b52a8fef17949
Guizar <alejandro.guizar@line-up.com.mx>
2022-12-08 14:10:27 -0600

    feature LMVP-447 Process card payments with 3DS
    https://nilo-innovation.atlassian.net/browse/LMVP-447
    
    - Create retrieve transaction information page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Transaction from "./Transaction"
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
       <Transaction/>
     </div>
   );
--- src/Transaction.jsx
+++ src/Transaction.jsx
@@ -1,41 +1,70 @@
-import {useState, useEffect} from "react" 
+// import {useState, useEffect} from "react" 
+import React from 'react';
 import axios from "axios"
 
-function Transaction () {
-const [transaction , setTransaction] = useState (null)
-const [formTransaction, setFormTransaction] = useState ({ 
-    status: "",
-    amount: 0.0,
-    description: ""
-})
 
-useEffect(() => {
-    getTransaction("trtxairtgbetuh2z6uxy")
-}, [])
+export default class Transaction extends React.Component {
 
-function getTransaction (transactionId) {
-    axios ({ 
-    method: "GET",
-    url:"/webhook-service/webhook/transaction/" + transactionId
-    }).then((response)=> {
-    const data = response.data
-    setTransaction(data)
-    }).catch((error) => {
-    if (error .response) {
-        console.log (error.response); 
-        console.log (error .response.status);
-        console.log (error.response.headers);
+    state = {
+        transaction: {
+            status: "",
+            amount: 0.0,
+            description: ""
+        }
     }
-    })
+// const [transaction , setTransaction] = useState (null)
+// const [formTransaction, setFormTransaction] = useState ({ 
+//     status: "",
+//     amount: 0.0,
+//     description: ""
+// })
+
+    componentDidMount() {
+        axios.get ("https://dev.lineupapp.digital/webhook-service/webhook/transaction/" + "trtxairtgbetuh2z6uxy", {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then((response)=> {
+            const transaction = response.data
+            console.log(transaction)
+            this.setState({transaction})
+            }).catch((error) => {
+            if (error .response) {
+                console.log (error.response); 
+                console.log (error.response.status);
+                console.log (error.response.headers);
+            }
+            })
+    }
+// useEffect(() => {
+//     getTransaction("trtxairtgbetuh2z6uxy")
+// }, {})
+
+// function getTransaction (transactionId) {
+//     axios ({ 
+//     method: "GET",
+//     url:"/webhook-service/webhook/transaction/" + transactionId
+//     }).then((response)=> {
+//     const data = response.data
+//     setTransaction(data)
+//     }).catch((error) => {
+//     if (error .response) {
+//         console.log (error.response); 
+//         console.log (error.response.status);
+//         console.log (error.response.headers);
+//     }
+//     })
+// }
+    render() {
+        return (
+            <div className="transaction">   
+                <h1> TransactionStatus: {this.state.transaction.status} </h1>
+                <p> Amount: {this.state.transaction.amount}</p>
+                <p> Description: {this.state.transaction.description}</p>
+            </div>
+        )
+    }
 }
 
-return (
-    <div className="transaction">   
-        <h1> TransactionStatus: {transaction.status} </h1>
-        <p> Amount: {transaction.amount}</p>
-        <p> Description: {transaction.description}</p>
-    </div>
-);
-}
-
-export default Transaction;
+// export default Transaction;
