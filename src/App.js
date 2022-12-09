66be15976dddd722cf0fb6aca43d6a14ba6b5909
Jorge Luis Calleja Alvarado <jorgelca1205@gmail.com>
2022-12-08 19:20:24 -0600

    feat: add reoter navigation an performance

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import './App.css';
-import Transaction from "./Transaction"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
 
+import "./App.css";
+import Transaction from "./Containers/Transaction";
+import NotFoundPage from "./Containers/NotFoundPage";
 
 function App() {
   return (
-    <div className="App">
-      <Transaction/>
-    </div>
+    <BrowserRouter>
+      <Helmet titleTemplate="%s - Transaction Status" defaultTitle="Innovation">
+        <meta name="description" content="A Transaction Status" />
+      </Helmet>
+
+      <Routes>
+        <Route path="/" element={<Transaction />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
